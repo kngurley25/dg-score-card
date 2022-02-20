ea8a9b3cbf2ec4ce41094c33058fe00eac89a27e
kngurley <kngurley25@gmail.com>
2022-02-20 11:51:27 -0600

    place hole schema in course model file, add virtual for hole count

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,17 +1,38 @@
 const { Schema, model } = require('mongoose');
-const holeSchema = require('./Hole');
 
-const courseSchema = new Schema({
-    courseName: {
-        type: String,
+const holeSchema = new Schema({
+    holeNumber: {
+        type: Number,
         required: true,
-        unique: true
     },
-    location: {
-        type: String,
-        require: true
+    par: {
+        type: Number,
+        required: true
+    }
+});
+
+const courseSchema = new Schema(
+    {
+        courseName: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        location: {
+            type: String,
+            require: true
+        },
+        holes: [holeSchema]
     },
-    holes: [holeSchema]
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+courseSchema.virtual('holeCount').get(function() {
+    return this.holes.length;
 });
 
 const Course = model('Course', courseSchema);
