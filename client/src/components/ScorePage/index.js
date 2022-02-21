280cd2d5363a041ac19ef919dcb56bacdb8b26b0
Adam Field <adamjfield@gmail.com>
2022-02-20 23:10:42 -0600

    added click handlers for adding and removing strokes

--- client/src/components/ScorePage/index.js
+++ client/src/components/ScorePage/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ScorePageClasses from './ScorePage.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function ScorePage() {
+  const [inputValue, setInputValue] = useState(1);
+
+  const addStroke = () => {
+    let score = document.getElementById('strokeTotal').value;
+    let newScore = ++score;
+    return setInputValue(newScore);
+  };
+
+  const removeStroke = () => {
+    let score = document.getElementById('strokeTotal').value;
+    if (score <= 1) {
+      alert('Score cannot be less than 1');
+      return;
+    }
+    let newScore = --score;
+    return setInputValue(newScore);
+  };
+
   return (
     <main className={ScorePageClasses.ScorePage}>
       <div className='d-flex flex-column align-items-center'>
@@ -12,29 +30,31 @@ function ScorePage() {
           <h2>Par 3</h2>
         </div>
         <p className='fs-3'>Enter Total Strokes:</p>
-        <button className='button-stroke text-center w-50'>
+        <button
+          id='addBtn'
+          className='button-stroke text-center w-50'
+          onClick={addStroke}
+        >
           <FontAwesomeIcon icon={faArrowUp} className='fs-1' />
         </button>
         <div className='d-flex justify-content-center my-2'>
-          <i class='fa fa-arrow-up' aria-hidden='true'></i>
-          {/* <label htmlFor='strokeTotal' /> */}
           <input
             type='number'
             pattern='[0-9]*'
             id='strokeTotal'
             min={1}
-            defaultValue={1}
             className='text-center w-50 fs-1 mt-3'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
           />
         </div>
-        <button className='button-stroke w-50'>
+        <button
+          id='subtractBtn'
+          className='button-stroke w-50'
+          onClick={removeStroke}
+        >
           <FontAwesomeIcon icon={faArrowDown} className='fs-1' />
         </button>
-        {/* <div>
-          <button className='button-stroke'>
-            <p>Add Stroke</p>
-          </button>
-        </div> */}
         <div>
           <button className='button-next my-5'>
             <p>Next Hole</p>
