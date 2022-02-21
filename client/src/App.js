1133c4d355602dac4c9020b57034a6456dbbad32
Adam Field <adamjfield@gmail.com>
2022-02-21 10:54:43 -0600

    add drop down nav menu

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,13 @@ import React from 'react';
 import Home from './Pages/Home';
 import './App.css';
 import ScorePage from './components/ScorePage';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <main>
         <Home></Home>
         <ScorePage></ScorePage>
